1b645a8a3445df568f8cef90d301ceff1a0b78ae
Bo4anckii <bobaanton@gmail.com>
2020-11-20 13:56:24 +0300

    Главный экран

--- src/crossword/Main.java
+++ src/crossword/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("resources/main_view.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Органайзер");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
--- src/crossword/ui/main/MainController.java
+++ src/crossword/ui/main/MainController.java
@@ -1,11 +1,22 @@
 package crossword.ui.main;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+    public TableView table;
+    public TableColumn timeColumn;
+    public TableColumn noteColumn;
+    public DatePicker datePicker;
+    public Button yesterdayBtn;
+    public Button tomorrowBtn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
